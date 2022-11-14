37a3acd45c7c1964702857da06a6b97a03c24f3f
Jose Ignacio <rivas.fujos20@triana.salesianos.edu>
2022-11-14 19:50:29 +0100

    filtrado por municipios terminado

--- GasAppV2/src/app/components/gasol-list/gasol-list.component.ts
+++ GasAppV2/src/app/components/gasol-list/gasol-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
-import { CarburanteResponse, ListaEESSPrecio } from 'src/app/interfaces/carburante.interface';
+import { ListaEESSPrecio } from 'src/app/interfaces/carburante.interface';
 import { MunicipioResponse } from 'src/app/interfaces/municipio.interface';
 import { ProvinciaResponse } from 'src/app/interfaces/provincia.interface';
 import { CarburanteService } from 'src/app/services/carburante.service';
@@ -15,11 +15,11 @@ import { CarburanteService } from 'src/app/services/carburante.service';
 export class GasolListComponent implements OnInit {
 
   myControl = new FormControl('');
-  filteredOptions?: Observable<MunicipioResponse[]>;
+  options: string[] = []
+  filteredOptions?: Observable<string[]>;
   gasolList: ListaEESSPrecio[] = [];
   gasolFilteredList: ListaEESSPrecio[] = [];
   provinciasList: ProvinciaResponse[] = [];
-  provinciaFilter: ProvinciaResponse[] = [];
   munList: MunicipioResponse[] = [];
   carburantesList = ['Gasoleo', 'Gasolina', 'Hidrogeno'];
   carburanteSelected = 'Gasoleo';
@@ -41,52 +41,41 @@ export class GasolListComponent implements OnInit {
     this.carburanteService.getProvincias().subscribe((resp) =>{
       this.provinciasList = resp;
     });
-
-    this.filteredOptions = this.myControl.valueChanges.pipe(
-      startWith(''),
-      map(value => this._filter(value || '')),
-    );
-
   }
 
-  private _filter(value: string): MunicipioResponse[] {
+  private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-
-    return this.munList.filter(option => option.Municipio.toLowerCase().includes(filterValue));
-  }
-
-  displayFn(m: MunicipioResponse): string {
-    return m.Municipio;
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   filtrar(){
+    this.options = [];
     this.gasolFilteredList = this.gasolList.filter(x => this.filtro(x));
   }
 
   filtro(x: ListaEESSPrecio): boolean {
+    debugger;
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+    debugger;
+    for (let it of this.gasolFilteredList) {
+      if(!this.options.includes(it['Municipio'])){
+        this.options.push(it['Municipio'])
+      }
+    }
+    debugger;
     let pasaFiltro = false;
     if(this.carburanteSelected == 'Gasoleo') {
-      pasaFiltro = +x['Precio Gasoleo A'].replace(",",".") < this.precioMax &&  this.provinciaSelected.includes(x['IDProvincia'])? true: false;
-      this.filtrarMunicipios(this.gasolFilteredList);
+      pasaFiltro = +x['Precio Gasoleo A'].replace(",",".") < this.precioMax &&  this.provinciaSelected.includes(x['IDProvincia']) ? true: false;
     } else if(this.carburanteSelected == 'Gasolina') {
       pasaFiltro = +x['Precio Gasolina 95 E5'].replace(",",".") < this.precioMax && this.provinciaSelected.includes(x['IDProvincia']) ? true: false;
-      this.filtrarMunicipios(this.gasolFilteredList);
     } else{
       pasaFiltro = +x['Precio Hidrogeno'].replace(",",".") < this.precioMax &&  this.provinciaSelected.includes(x['IDProvincia'])? true: false;
-      this.filtrarMunicipios(this.gasolFilteredList);
     }
     return pasaFiltro;
   }
 
-  filtrarMunicipios(gasol: ListaEESSPrecio[]) {
-    this.gasolFilteredList = gasol.filter(x => x.IDMunicipio == this.municipioSelected);
-  }
-  
-  getMunicipios(){
-    this.munList = [];
-    this.provinciaSelected.forEach(x => this.carburanteService.getMunicipios(x).subscribe((resp) => {
-      this.munList = this.munList.concat(resp);
-    }))
-  }
 }
 
